aa9d7f59373898ba9f064c5c682c071740a37224
Christian Rickert <rc.login@icloud.com>
2024-03-20 16:19:18 -0600

    pin versions
    set minimum R requirement to 4.3.3 and pin recent versions of package dependencies

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,7 @@
+#remove.packages("phenoptrReports")
+devtools::install_github("christianrickert/phenoptrReports")
+#devtools::install_local("C:/Users/Christian Rickert/Documents/GitHub/phenoptrReports")
+
 library(testthat)
 library(phenoptrReports)
 library(rstudioapi)
